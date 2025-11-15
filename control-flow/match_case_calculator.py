5a248603d1e33a036583df22f49cce6389f043de
evansboateng954 <evansboateng954@gmail.com>
2025-11-14 22:31:07 -0800

    match_case_calculator.py

---
+++ control-flow/match_case_calculator.py
@@ -0,0 +1,22 @@
+first = int(input("Enter the first number:"))
+second = int(input("Enter the second number:"))
+operation = input("Chose the operation(+, -, *, /):")
+
+match operation:
+    case "+":
+        print(f" The result is {first + second}") 
+
+    case "*":
+        print(f" The result is {first * second}")
+
+    case "-":
+        print(f" The result is {first - second}")
+
+    case "/":
+        if second == 0:
+            print("Cannot divide by zero")
+        else:
+            print(f" The result is {first / second}")
+
+    case _:
+        print("Invalid operator")
