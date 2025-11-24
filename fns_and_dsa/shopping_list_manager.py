9f4a2174fc78722a5a90f583bb4dc95b7baac266
evansboateng954 <evansboateng954@gmail.com>
2025-11-23 23:41:02 -0800

    shopping_list_manager.py

---
+++ fns_and_dsa/shopping_list_manager.py
@@ -0,0 +1,32 @@
+def display_menu():
+    print("Shopping List Manager")
+    print("1. Add Item")
+    print("2. Remove Item")
+    print("3. View List")
+    print("4. Exit")
+
+def main():
+    shopping_list = []
+    while True:
+        display_menu()
+        choice = input("Enter your choice: ")
+
+        if choice == '1':
+          item_name = input("Enter name of item")
+          shopping_list.append(item_name)
+          
+        elif choice == '2':
+          item_name = input("Enter name of item")
+          shopping_list.remove(item_name)
+
+        elif choice == '3':
+            print(list(shopping_list))
+
+        elif choice == '4':
+            print("Goodbye!")
+            break
+        else:
+            print("Invalid choice. Please try again.")
+
+if __name__ == "__main__":
+    main()
