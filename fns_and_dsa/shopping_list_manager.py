5f086c2e4aff081f49b07e2562f8e9b5935eefd5
evansboateng954 <evansboateng954@gmail.com>
2025-11-23 23:45:20 -0800

    shopping_list_manager.py

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -12,7 +12,7 @@ def main():
         choice = input("Enter your choice: ")
 
         if choice == '1':
-          item_name = input("Enter item to add")
+            item_name = input("Enter item to add: ")
           shopping_list.append(item_name)
           
         elif choice == '2':
