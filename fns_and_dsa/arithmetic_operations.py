e621f6417f478b384e6153fde04b4bab3d5ee260
evansboateng954 <evansboateng954@gmail.com>
2025-11-23 23:06:08 -0800

    arithmetic_operations.py

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -12,6 +12,8 @@ def perform_operation(num1, num2, operation):
         case "divide":
             if num2 == 0:
                 return("cannot divide by zero")
+            elif:
+                pass
             return num1/ num2
         case _:
             return("Invalid operation")
