ae9ac36a8df364034a48f4811485b6b223e1f3fe
evansboateng954 <evansboateng954@gmail.com>
2025-12-30 15:14:18 -0800

    temp_conversion_tool.py

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -2,21 +2,21 @@ FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
 CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
 
 def convert_to_celsius(fahrenheit):
-    global FARENHEIT_TO_CELSIUS_FACTOR
-    return temperature *  FARENHEIT_TO_CELCIUS_FACTOR
+    global FAHRENHEIT_TO_CELSIUS_FACTOR
+    return temperature *  FAHRENHEIT_TO_CELSIUS_FACTOR
 
 def convert_to_fahrenheit(celsius):
     global CELSIUS_TO_FAHRENHEIT_FACTOR
     return temperature * CELSIUS_TO_FAHRENHEIT_FACTOR
 
 temperature = float(input("Enter temperature to convert: "))
-unit = input("Is this temperature in Celsius or Fahrenheit? (C/F):")
+unit = input("Is this temperature in Celsius or Fahrenheit? (C/F): ")
 
-if unit == "F":
-    print(convert_to_fahrenheit(temperature))
+if unit == "C":
+    print(f"{temperature}°C is {convert_to_fahrenheit(temperature)}°F")
 
-elif unit =="C":
-    print(convert_to_celsius(temperature))
+elif unit =="F":
+    print(f"{temperature}°F is {convert_to_celsius(temperature)}°C")
 
 else:
     print("Invalid unit")
